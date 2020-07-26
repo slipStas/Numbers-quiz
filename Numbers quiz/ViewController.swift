70e890849f63c0e2bdd7925673eb906c82ffca4e
slipStas <41761920+slipStas@users.noreply.github.com>
2020-07-26 14:29:35 +0300

    start

---
+++ Numbers quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Numbers quiz
+//
+//  Created by Stanislav Slipchenko on 26.07.2020.
+//  Copyright © 2020 Stanislav Slipchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
