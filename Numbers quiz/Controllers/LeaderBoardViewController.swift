69fe8ac15aadeebabdf575923778c202a32201fa
slipStas <41761920+slipStas@users.noreply.github.com>
2020-08-08 04:15:09 +0300

    add leader board table view controller

---
+++ Numbers quiz/Controllers/LeaderBoardViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LeaderBoardViewController.swift
+//  Numbers quiz
+//
+//  Created by Stanislav Slipchenko on 08.08.2020.
+//  Copyright © 2020 Stanislav Slipchenko. All rights reserved.
+//
+
+import UIKit
+
+class LeaderBoardViewController: UIViewController {
+
+    @IBOutlet weak var leaderBoardTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.leaderBoardTableView.dataSource = self
+    }
+    
+}
+
+extension LeaderBoardViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = leaderBoardTableView.dequeueReusableCell(withIdentifier: "leaderboardCell", for: indexPath) as! LeaderBoardTableViewCell
+        
+        cell.leaderBoardLabel.text = "number " + String(indexPath.row)
+        
+        return cell
+    }
+    
+    
+}
---
+++ Numbers quiz/Table cells/LeaderBoardTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  LeaderBoardTableViewCell.swift
+//  Numbers quiz
+//
+//  Created by Stanislav Slipchenko on 08.08.2020.
+//  Copyright © 2020 Stanislav Slipchenko. All rights reserved.
+//
+
+import UIKit
+
+class LeaderBoardTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var leaderBoardLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
